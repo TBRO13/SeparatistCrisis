58a0d426c732794c3188fdc76c290f01bc8bf1a2
Kevin Price <kevinpricethesecond@gmail.com>
2023-06-23 14:52:01 -0500

    cleanup

--- AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyCompany("SeparatistCrisis")]
-[assembly: AssemblyConfiguration("Debug")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
-[assembly: AssemblyProduct("SeparatistCrisis")]
-[assembly: AssemblyTitle("SeparatistCrisis")]
-[assembly: AssemblyVersion("1.0.0.0")]
